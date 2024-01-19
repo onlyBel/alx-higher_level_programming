226420cea501db6fa6a9e2c599b7dbb258862b0c
onlyBel <belindawnjr@gmail.com>
2024-01-18 18:44:58 -0800

    ✓

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = [1, 2, 3]
-print_reversed_list_integer(my_list)
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+    
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
