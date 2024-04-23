135e5d9006f8195580639058ff852f303a6e744b
onlyBel <belindawnjr@gmail.com>
2024-04-23 20:00:28 +0000

    0x13-js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const prevSquare = require('./5-square.js');
+
+class Square extends prevSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
