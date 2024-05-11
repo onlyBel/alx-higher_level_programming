03d2c472221499bb3d192a3830f25b7fe09dd07a
onlyBel <belindawnjr@gmail.com>
2024-05-11 02:57:40 +0000

    0x11py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """Fetch
-https://intranet.hbtn.io/status
+https://alx-intranet.hbtn.io/status
 using urlib package
 """
-
 import urllib.request
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as request:
         response = request.read()
         print("Body response:")
         print("\t- type: {}".format(type(response)))
